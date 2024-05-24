1349136569b57dfc58707d62eccd51b75c8a5410
Josh Yim <jyim@lcs.on.ca>
2024-05-24 11:40:38 -0400

    Initial Commit

---
+++ LCSalumni/LCSalumniApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCSalumniApp.swift
+//  LCSalumni
+//
+//  Created by junxi Yim on 2024/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LCSalumniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
