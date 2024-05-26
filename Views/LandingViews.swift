043ea14837d017cbf724e9a667121641396b836f
Josh Yim <jyim@lcs.on.ca>
2024-05-26 14:25:04 -0400

    Added Searchable to show the search bar on Menu view

--- Views/LandingViews.swift
+++ Views/LandingViews.swift
@@ -8,61 +8,65 @@
 import SwiftUI
 
 struct LandingViews: View {
+    @State var searchText = ""
     var body: some View {
         let twoRows  = [GridItem(), GridItem()]
         NavigationStack{
             VStack {
-               
-                VStack(alignment: .leading){
-                    HStack {
-                        Text("Recent Grads")
-                            .font(.system(size: 25))
-                            .padding(.bottom,5)
-                            .bold()
-                        Image(systemName: "chevron.right")
-                            .resizable()
-                            .frame(width: 10,height:22)
-                            .bold()
-                    }
-                    
-                    
-                    
-                    ScrollView(.horizontal) {
-                        LazyHGrid(rows: twoRows,spacing: 10) {
-                            ForEach(recentlyGrads) {currentStudent in
-                                basicStudentInfo(Person:currentStudent)
-                                
+                ScrollView(.vertical){
+                    VStack(alignment: .leading){
+                        HStack {
+                            Text("Recent Grads")
+                                .font(.system(size: 25))
+                                .padding(.bottom,5)
+                                .bold()
+                            Image(systemName: "chevron.right")
+                                .resizable()
+                                .frame(width: 10,height:22)
+                                .bold()
+                        }
+                        
+                        
+                        
+                        ScrollView(.horizontal) {
+                            LazyHGrid(rows: twoRows,spacing: 10) {
+                                ForEach(recentlyGrads) {currentStudent in
+                                    basicStudentInfo(Person:currentStudent)
+                                    
+                                }
                             }
                         }
+                        .frame(height:270)
+                        
+                        HStack{
+                            Text("Famous Alumni")
+                                .font(.system(size: 25))
+                                .bold()
+                            Image(systemName: "chevron.right")
+                                .resizable()
+                                .frame(width: 10,height:22)
+                                .bold()
+                        }
+                        .padding(.vertical,5)
+                        ScrollView(.horizontal) {
+                            LazyHGrid(rows: twoRows,spacing: 10) {
+                                ForEach(FamousAlumni) {currentStudent in
+                                    basicStudentInfo(Person:currentStudent)
+                                    
+                                }
+                            }
+                        }
+                        .frame(height:270)
+                        
                     }
-                    .frame(height:270)
-                  
-                    HStack{
-                        Text("Famous Alumni")
-                            .font(.system(size: 25))
-                            .bold()
-                        Image(systemName: "chevron.right")
-                            .resizable()
-                            .frame(width: 10,height:22)
-                            .bold()
-                    }
-                    .padding(.vertical,5)
-                    .border(.red)
-                    ScrollView(.horizontal) {
-                                            LazyHGrid(rows: twoRows,spacing: 10) {
-                                                ForEach(FamousAlumni) {currentStudent in
-                                                    basicStudentInfo(Person:currentStudent)
-                                                    
-                                                }
-                                            }
-                                        }
-                    
+                    .padding(.leading,10)
+                    Spacer()
                 }
-                .padding(.leading,10)
-                
             }
             .navigationTitle("Title to be determined")
             .navigationBarTitleDisplayMode(.inline)
+            .searchable(text: $searchText)
+                            .padding(.leading,10)
                     
                 }
                 
